cd16d98c0ea72f221ac1c2fc6f9ce73367e71e59
DanielM2292 <143299403+DanielM2292@users.noreply.github.com>
2024-12-27 21:08:03 -0500

    Cambios autenticacion rol

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,138 +1,226 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
-    CaretDown,
-    CaretLeft,
-    Invoice,
-    Calculator,
-    SignOut,
-    FilePlus,
-    User,
-    MoneyWavy,
-    FolderPlus,
-    Wallet,
-    Plus,
-    TreasureChest,
-    Minus,
+  CaretDown,
+  CaretLeft,
+  Invoice,
+  Calculator,
+  SignOut,
+  FilePlus,
+  User,
+  MoneyWavy,
+  FolderPlus,
+  Wallet,
+  Plus,
+  TreasureChest,
+  Minus,
 } from "@phosphor-icons/react";
 import AcueductoLogo from "../imagenes/LogoAcueducto.png";
 
 const Sidebar = () => {
-    const [activeItems, setActiveItems] = useState([]);
-    const [isSidebarActive, setSidebarActive] = useState(false);
+  const [activeItems, setActiveItems] = useState([]);
+  const [isSidebarActive, setSidebarActive] = useState(false);
+  const [userRole, setUserRole] = useState("");
 
-    const handleItemClick = (index) => {
-        setActiveItems((prev) =>
-            prev.includes(index)
-                ? prev.filter((item) => item !== index)
-                : [...prev, index]
-        );
-    };
+  useEffect(() => {
+    const role = localStorage.getItem("userRole");
+    setUserRole(role);
+  }, []);
 
-    const toggleSidebar = () => {
-        setSidebarActive((prev) => !prev);
-    };
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-    };
-
-    return (
-        <div className={`Sidebar ${isSidebarActive ? "active" : ""}`}>
-            <div className="menuBtn" onClick={toggleSidebar}>
-                <CaretLeft className="iconArrow" />
-            </div>
-            <div className="navSidebar">
-                <div className="AcueductoMenu">
-                    <div className={`logo-container ${isSidebarActive ? "open" : "closed"}`}>
-                        <img src={AcueductoLogo} alt="Logo Acueducto" className="logo" />
-                        {!isSidebarActive && <p className="SideBarTittle">Agua Pura, Vida Segura</p>}
-                    </div>
-
-                    <ul>
-                        <li className={activeItems.includes(1) ? "active" : ""}>
-                            <Link to="#" onClick={() => handleItemClick(1)}>
-                                <Invoice className="iconSidebar" />
-                                <span className="text">Facturación</span>
-                                <CaretDown
-                                    className={`arrow ${activeItems.includes(1) ? "active" : ""}`}
-                                />
-                            </Link>
-                            <ul className={`submenu ${activeItems.includes(1) ? "open" : ""}`}>
-                                <li>
-                                    <Link to="/app/facturacion" onClick={() => setActiveItems([])}>
-                                        <FilePlus className="iconSidebar" />
-                                        <span className="text">Crear Factura</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/clientes" onClick={() => setActiveItems([])}>
-                                        <User className="iconSidebar" />
-                                        <span className="text">Clientes</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/multas" onClick={() => setActiveItems([])}>
-                                        <MoneyWavy className="iconSidebar" />
-                                        <span className="text">Multas</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/matriculas" onClick={() => setActiveItems([])}>
-                                        <FolderPlus className="iconSidebar" />
-                                        <span className="text">Matrículas</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/pagos" onClick={() => setActiveItems([])}>
-                                        <Wallet className="iconSidebar" />
-                                        <span className="text">Pagos</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className={activeItems.includes(2) ? "active" : ""}>
-                            <Link to="#" onClick={() => handleItemClick(2)}>
-                                <Calculator className="iconSidebar" />
-                                <span className="text">Contabilidad</span>
-                                <CaretDown
-                                    className={`arrow ${activeItems.includes(2) ? "active" : ""}`}
-                                />
-                            </Link>
-                            <ul className={`submenu ${activeItems.includes(2) ? "open" : ""}`}>
-                                <li>
-                                    <Link to="/app/ingresos" onClick={() => setActiveItems([])}>
-                                        <Plus className="iconSidebar" />
-                                        <span className="text">Ingresos</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/egresos" onClick={() => setActiveItems([])}>
-                                        <Minus className="iconSidebar" />
-                                        <span className="text">Egresos</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/app/inventario" onClick={() => setActiveItems([])}>
-                                        <TreasureChest className="iconSidebar" />
-                                        <span className="text">Inventario</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <div className="logout-container">
-                            <li className="logout-button">
-                                <Link to="/" onClick={handleLogout}>
-                                    <SignOut className="iconSidebar" />
-                                    <span className="text">Salir</span>
-                                </Link>
-                            </li>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-        </div>
+  const handleItemClick = (index) => {
+    setActiveItems((prev) =>
+      prev.includes(index)
+        ? prev.filter((item) => item !== index)
+        : [...prev, index]
     );
+  };
+
+  const toggleSidebar = () => {
+    setSidebarActive((prev) => !prev);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userRole');
+  };
+
+  return (
+    <div className={`Sidebar ${isSidebarActive ? "active" : ""}`}>
+      <div className="menuBtn" onClick={toggleSidebar}>
+        <CaretLeft className="iconArrow" />
+      </div>
+      <div className="navSidebar">
+        <div className="AcueductoMenu">
+          <div className={`logo-container ${isSidebarActive ? "open" : "closed"}`}>
+            <img src={AcueductoLogo} alt="Logo Acueducto" className="logo" />
+            {!isSidebarActive && <p className="SideBarTittle">Agua Pura, Vida Segura</p>}
+          </div>
+
+          <ul>
+            {userRole === "1" && (
+              <>
+                <li className={activeItems.includes(1) ? "active" : ""}>
+                  <Link to="#" onClick={() => handleItemClick(1)}>
+                    <Invoice className="iconSidebar" />
+                    <span className="text">Facturación</span>
+                    <CaretDown
+                      className={`arrow ${activeItems.includes(1) ? "active" : ""}`}
+                    />
+                  </Link>
+                  <ul className={`submenu ${activeItems.includes(1) ? "open" : ""}`}>
+                    <li>
+                      <Link to="/app/facturacion" onClick={() => setActiveItems([])}>
+                        <FilePlus className="iconSidebar" />
+                        <span className="text">Crear Factura</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/clientes" onClick={() => setActiveItems([])}>
+                        <User className="iconSidebar" />
+                        <span className="text">Clientes</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/multas" onClick={() => setActiveItems([])}>
+                        <MoneyWavy className="iconSidebar" />
+                        <span className="text">Multas</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/matriculas" onClick={() => setActiveItems([])}>
+                        <FolderPlus className="iconSidebar" />
+                        <span className="text">Matrículas</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/pagos" onClick={() => setActiveItems([])}>
+                        <Wallet className="iconSidebar" />
+                        <span className="text">Pagos</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className={activeItems.includes(2) ? "active" : ""}>
+                  <Link to="#" onClick={() => handleItemClick(2)}>
+                    <Calculator className="iconSidebar" />
+                    <span className="text">Contabilidad</span>
+                    <CaretDown
+                      className={`arrow ${activeItems.includes(2) ? "active" : ""}`}
+                    />
+                  </Link>
+                  <ul className={`submenu ${activeItems.includes(2) ? "open" : ""}`}>
+                    <li>
+                      <Link to="/app/ingresos" onClick={() => setActiveItems([])}>
+                        <Plus className="iconSidebar" />
+                        <span className="text">Ingresos</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/egresos" onClick={() => setActiveItems([])}>
+                        <Minus className="iconSidebar" />
+                        <span className="text">Egresos</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/app/inventario" onClick={() => setActiveItems([])}>
+                        <TreasureChest className="iconSidebar" />
+                        <span className="text">Inventario</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              </>
+            )}
+
+            {userRole === "2" && (
+              <li className={activeItems.includes(2) ? "active" : ""}>
+                <Link to="#" onClick={() => handleItemClick(2)}>
+                  <Calculator className="iconSidebar" />
+                  <span className="text">Contabilidad</span>
+                  <CaretDown
+                    className={`arrow ${activeItems.includes(2) ? "active" : ""}`}
+                  />
+                </Link>
+                <ul className={`submenu ${activeItems.includes(2) ? "open" : ""}`}>
+                  <li>
+                    <Link to="/app/ingresos" onClick={() => setActiveItems([])}>
+                      <Plus className="iconSidebar" />
+                      <span className="text">Ingresos</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/egresos" onClick={() => setActiveItems([])}>
+                      <Minus className="iconSidebar" />
+                      <span className="text">Egresos</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/inventario" onClick={() => setActiveItems([])}>
+                      <TreasureChest className="iconSidebar" />
+                      <span className="text">Inventario</span>
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            )}
+
+            {userRole === "3" && (
+              <li className={activeItems.includes(1) ? "active" : ""}>
+                <Link to="#" onClick={() => handleItemClick(1)}>
+                  <Invoice className="iconSidebar" />
+                  <span className="text">Facturación</span>
+                  <CaretDown
+                    className={`arrow ${activeItems.includes(1) ? "active" : ""}`}
+                  />
+                </Link>
+                <ul className={`submenu ${activeItems.includes(1) ? "open" : ""}`}>
+                  <li>
+                    <Link to="/app/facturacion" onClick={() => setActiveItems([])}>
+                      <FilePlus className="iconSidebar" />
+                      <span className="text">Crear Factura</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/clientes" onClick={() => setActiveItems([])}>
+                      <User className="iconSidebar" />
+                      <span className="text">Clientes</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/multas" onClick={() => setActiveItems([])}>
+                      <MoneyWavy className="iconSidebar" />
+                      <span className="text">Multas</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/matriculas" onClick={() => setActiveItems([])}>
+                      <FolderPlus className="iconSidebar" />
+                      <span className="text">Matrículas</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/app/pagos" onClick={() => setActiveItems([])}>
+                      <Wallet className="iconSidebar" />
+                      <span className="text">Pagos</span>
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            )}
+
+            <div className="logout-container">
+              <li className="logout-button">
+                <Link to="/" onClick={handleLogout}>
+                  <SignOut className="iconSidebar" />
+                  <span className="text">Salir</span>
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,53 +1,114 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import AcueductoLogo from '../imagenes/LogoAcueducto.png'
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AcueductoLogo from '../imagenes/LogoAcueducto.png';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rol, setRol] = useState('1'); 
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const navigate = useNavigate();
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-    const UsuarioPredefinido = {
-        usuario: 'admin',
-        password: '123456'
-    }
-
-    const handleLogin = (event) => {
-        event.preventDefault()
-        if (username === UsuarioPredefinido.usuario && password === UsuarioPredefinido.password) {
-            alert('Sesión iniciada con éxito')
-            navigate('/app')
-        } else {
-            setError('Usuario o contraseña incorrectos')
+    try {
+      const formData = new FormData();
+      formData.append('email', username);
+      formData.append('password', password);
+      const response = await fetch('http://localhost:9090/verify_role', {
+        method: 'POST',
+        body: formData,
+        credentials: 'include',
+        headers: {
+          'Accept': 'application/json'
         }
+      });
 
+      if (response.ok) {
+        const data = await response.json();
+        const realRole = data.rol;
+
+        if (realRole !== rol) {
+          alert("El rol seleccionado no coincide con el rol real del usuario. Por favor, selecciona el rol correcto.");
+          return;
+        }
+        formData.append('rol', rol);
+        const loginResponse = await fetch('http://localhost:9090/login', {
+          method: 'POST',
+          body: formData,
+          credentials: 'include',
+          headers: {
+            'Accept': 'application/json'
+          }
+        });
+
+        if (loginResponse.ok) {
+          localStorage.setItem('userRole', rol);
+          navigate('/app');
+        } else {
+          setError('Usuario o contraseña incorrectos');
+        }
+      } else {
+        setError('Usuario o contraseña incorrectos');
+      }
+    } catch (error) {
+      setError('Usuario o contraseña incorrectos');
     }
+  };
 
-    return (
-        <div>
-            <div className='logoContainer'>
-                <img src={AcueductoLogo} alt='Acueducto Logo' className='logo' />
-                <h1 className='textoLogo'>Agua Pura, Vida Segura</h1>
-            </div>
-            <div className="loginContainer">
-                <h2 className="loginTitle">Iniciar Sesión</h2>
-                <form className="loginForm" onSubmit={handleLogin}>
-                    <div className="inputGroup">
-                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                        <label>Usuario</label>
-                    </div>
-                    <div className="inputGroup">
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                        <label>Contraseña</label>
-                    </div>
-                    <button type="submit" className="loginButton">Iniciar Sesión</button>
-                </form>
-            </div>
+  return (
+    <div>
+      <div className='logoContainer'>
+        <img src={AcueductoLogo} alt='Acueducto Logo' className='logo' />
+        <h1 className='textoLogo'>Agua Pura, Vida Segura</h1>
+      </div>
+      <div className="loginContainer">
+        <h2 className="loginTitle">Iniciar Sesión</h2>
+        <form className="loginForm" onSubmit={handleLogin}>
+          <div className={`inputGroup ${error ? 'error' : ''}`}>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              autoComplete="username"
+            />
+            <label>Usuario</label>
+          </div>
+          <div className={`inputGroup ${error ? 'error' : ''}`}>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="current-password"
+            />
+            <label>Contraseña</label>
+          </div>
+          <div className="inputGroup">
+            <select
+              className="styledSelect"
+              value={rol}
+              onChange={(e) => setRol(e.target.value)}
+              required
+            >
+              <option value="1">Administrador</option>
+              <option value="3">Auxiliar</option>
+              <option value="2">Contador</option>
+            </select>
+          </div>
+          <button type="submit" className="loginButton">
+            Iniciar Sesión
+          </button>
+          {error && <p className="error-message">{error}</p>}
+        </form>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-export default LoginPage
+export default LoginPage;
+
+
